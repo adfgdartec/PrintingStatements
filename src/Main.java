42ed689579b0d9f82930bfd9e83ea4a4fd88df5b
Adit Rajaram <rajitadarth422@gmail.com>
2024-07-23 11:15:47 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public record Main() {
+    public static void main(String[] args) {
+        // Prints the name of my favorite character from my favorite book!
+        // This character is from the book The Speaker, the second book of sea of ink and gold.
+        System.out.print("Archer! ");
+        System.out.print("He couldn't speak in the first book!");
+        System.out.println("Archer likes:  \nFighting \n Family \n His Friends");
+        System.out.println("Hello,\tiD Tech!");
+        System.out.println("Hello, iD Tech!\b\b\b");
+
+    }
+}
